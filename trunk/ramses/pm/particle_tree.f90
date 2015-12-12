71bb20d5f3ca8e49e37da02f5dd1d3711b86c221
Andreas Bleuler <ableuler@physik.uzh.ch>
2015-12-12 01:59:09 +0100

    cleaning

--- trunk/ramses/pm/particle_tree.f90
+++ trunk/ramses/pm/particle_tree.f90
@@ -415,18 +415,6 @@ subroutine get_cell_index_from_hilbertkey(cell_index,cell_levl,hilbert_key2,hilb
   integer :: i
   call hilbert3d_reverse(x,y,z,hilbert_key2,hilbert_key1,hilbert_key0,ilevel,np)
   call get_cell_index_from_cartesian_hash(cell_index,cell_levl,x,y,z,ilevel,np,ilevel)
-  ! call get_cell_index_from_cartesian(cell_index2,cell_levl2,x,y,z,ilevel,np,ilevel)
-
-  ! do i = 1, np
-  !    if (cell_index(i) .ne. cell_index2(i) .or. cell_levl(i) .ne. cell_levl2(i) ) then
-  !       print*, 'problem in cind:', i
-  !       print*, x(i),y(i),z(i)
-  !       print*, cell_index(i), cell_index2(i)
-  !       print*,cell_levl(i), cell_levl2(i), ilevel
-  !       print*,'xg',xg(mod(cell_index2(i) - ncoarse,ngridmax),1:3) 
-  !       stop
-  !    end if
-  ! end do
      
 end subroutine get_cell_index_from_hilbertkey
 
